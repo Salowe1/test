850f7721652949229494244dcb0f7214391d87de
Salowe Cedric Martinien Adouabou <95149239+Salowe1@users.noreply.github.com>
2024-08-21 11:30:05 +0000

    Update logout.dart

--- lib/screens/settings/logout.dart
+++ lib/screens/settings/logout.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:test/constants/colors.dart';
 import 'package:test/screens/auth/login/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
- // Make sure to import your AuthService
+// Make sure to import your AuthService
 
 class LogoutDialog extends StatelessWidget {
   const LogoutDialog({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final FirebaseAuth _auth = FirebaseAuth.instance;
+    // final FirebaseAuth _auth = FirebaseAuth.instance;
 
     return AlertDialog(
       backgroundColor: Colors.white,
@@ -36,9 +36,9 @@ class LogoutDialog extends StatelessWidget {
         const SizedBox(width: 60),
         TextButton(
           onPressed: () async {
-            await _auth.signOut();
-            SharedPreferences prefs = await SharedPreferences.getInstance();
-            await prefs.setBool('isLoggedIn', false);
+            // await _auth.signOut();
+            // SharedPreferences prefs = await SharedPreferences.getInstance();
+            // await prefs.setBool('isLoggedIn', false);
 
             Get.offAll(() => const LoginScreen()); // Navigate to the login screen after logout
           },
