088a68c148f45e9f00c92f7d7f709f7d2c5f1557
Salowe Cedric Martinien Adouabou <95149239+Salowe1@users.noreply.github.com>
2024-09-08 01:35:30 +0000

    Update app_route.dart

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,4 +1,3 @@
-
 import 'package:get/get.dart';
 import 'package:test/screens/home/home.dart';
 import 'package:test/screens/settings/password/change_password.dart';
@@ -27,14 +26,11 @@ import 'package:test/screens/auth/onboard.dart'; // Import onboarding screens
 import 'package:test/screens/auth/first_page.dart'; // Import onboarding screens
 import 'route.dart';
 
-
 class Routes {
   static final routes = [
-    
     GetPage(name: '/', page: () => SplashScreen()),
     GetPage(name: MyRoutes.onboardingScreen, page: () => OnboardingScreen()),
-   
-    GetPage(name: MyRoutes.firstPage, page: () => FirstPageScreen()), 
+    GetPage(name: MyRoutes.firstPage, page: () => FirstPageScreen()),
     GetPage(name: MyRoutes.home, page: () => const HomeScreen()),
     GetPage(name: MyRoutes.phoneNumber, page: () => const PhoneNumberScreen()),
     GetPage(name: MyRoutes.settings, page: () => const SettingsScreen()),
@@ -51,12 +47,17 @@ class Routes {
     GetPage(name: MyRoutes.myEcarte, page: () => const MyEcarteScreen()),
     GetPage(name: MyRoutes.ecarteOrder, page: () => const EcarteOrderScreen()),
     GetPage(name: MyRoutes.login, page: () => const LoginScreen()),
-    GetPage(name: MyRoutes.completeProfile, page: () => CompleteProfileScreen()), // Add this line
+    GetPage(name: MyRoutes.completeProfile, page: () => CompleteProfileScreen()),
     GetPage(name: MyRoutes.photoCnib, page: () => PhotoCnib()),
-    GetPage(name: '/test-storage', page: () => TestStorage()),  // Add the route
-    GetPage(name: MyRoutes.senderNumberScreen, page: () => SenderNumberScreen()),
-    GetPage(name: MyRoutes.amountToSend, page: () => AmountToSendScreen(selectedNumber: '',)),
+    GetPage(name: '/test-storage', page: () => TestStorage()),
+    GetPage(
+      name: MyRoutes.senderNumberScreen,
+      page: () => SenderNumberScreen(),
+    ),
+    GetPage(
+      name: MyRoutes.amountToSend,
+      page: () => AmountToSendScreen(finalAmount: 0), // Default value or replace with appropriate value
+    ),
     GetPage(name: MyRoutes.paymentMethods, page: () => PaymentMethodsScreen()),
-  
   ];
 }
